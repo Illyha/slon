2bb13fbd55ee7a8b43c7d3518e73d1a90e80de88
Anonymous <user@rivne.itstep.org>
2019-06-10 20:35:20 +0300

    second commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,15 +2,21 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class Home extends Component {
-    state = {  }
+    state = {}
     render() {
-        console.log ('---Home props---',this.props); 
-        return ( 
+        console.log('---Home props---', this.props);
+        return (
             <div>
                 <h1>Home Page</h1>
             </div>
-         );
+        );
     }
 }
- 
+const mapStateProps = (state) => {
+    console.log('----redux store connect----', state);
+    return {
+        count: state.counter.counterStore
+    };
+}
+export default connect(mapStateProps)(Home);
 export default connect()(Home);
